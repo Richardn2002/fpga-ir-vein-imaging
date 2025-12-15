234cacac47362777084f048aa9f8d500df00f790
Jack Coursen <jcoursen2@gmail.com>
2025-12-15 09:05:13 -0500

    Add files via upload

--- HDL/VHDL/hessian_conv/HESSIAN_CONV_C_TB.vhd
+++ HDL/VHDL/hessian_conv/HESSIAN_CONV_C_TB.vhd
@@ -4,24 +4,24 @@ USE IEEE.numeric_std.ALL;
 USE STD.textio.ALL;
 USE IEEE.std_logic_textio.ALL;
 
-ENTITY tb_HESSIAN_conv_r IS
+ENTITY tb_HESSIAN_conv_c IS
     GENERIC (
-        INPUT_FILE   : STRING := "C:\Users\cours\IR_IMAGING\HDL_TB\stage02_clahe_output.txt";
-        OUTPUT_FILE  : STRING := "C:\Users\cours\IR_IMAGING\PL_hessianCONVR_output_90x90.txt"
+        INPUT_FILE  : STRING := "C:\Users\cours\IR_IMAGING\HDL_TB\stage03_conv_r.txt";
+        OUTPUT_FILE : STRING := "C:\Users\cours\IR_IMAGING\PP_hessianCONVC_output_90x90.txt"
     );
-END tb_HESSIAN_conv_r;
+END tb_HESSIAN_conv_c;
 
-ARCHITECTURE arch OF tb_HESSIAN_conv_r IS
+ARCHITECTURE arch OF tb_HESSIAN_conv_c IS
 
-    COMPONENT HESSIAN_conv_r
+    COMPONENT HESSIAN_conv_c
         PORT (
             clk           : IN  STD_LOGIC;
             trg           : IN  STD_LOGIC;
             rdy           : OUT STD_LOGIC;
 
-            clahe_in_en   : OUT STD_LOGIC;
-            clahe_in_addr : OUT STD_LOGIC_VECTOR(13 DOWNTO 0);
-            clahe_in_d    : IN  STD_LOGIC_VECTOR(7 DOWNTO 0);
+            conv_in_en    : OUT STD_LOGIC;
+            conv_in_addr  : OUT STD_LOGIC_VECTOR(12 DOWNTO 0);
+            conv_in_d     : IN  STD_LOGIC_VECTOR(15 DOWNTO 0);
 
             conv_out_en   : OUT STD_LOGIC;
             conv_out_addr : OUT STD_LOGIC_VECTOR(12 DOWNTO 0);
@@ -29,23 +29,25 @@ ARCHITECTURE arch OF tb_HESSIAN_conv_r IS
         );
     END COMPONENT;
 
+
     SIGNAL clk : STD_LOGIC := '0';
     CONSTANT clk_period : TIME := 10 ns;
 
     SIGNAL trg           : STD_LOGIC := '0';
     SIGNAL rdy           : STD_LOGIC;
 
-    SIGNAL clahe_in_en   : STD_LOGIC;
-    SIGNAL clahe_in_addr : STD_LOGIC_VECTOR(13 DOWNTO 0);
-    SIGNAL clahe_in_d    : STD_LOGIC_VECTOR(7 DOWNTO 0);
+    SIGNAL conv_in_en    : STD_LOGIC;
+    SIGNAL conv_in_addr  : STD_LOGIC_VECTOR(12 DOWNTO 0);
+    SIGNAL conv_in_d     : STD_LOGIC_VECTOR(15 DOWNTO 0);
 
     SIGNAL conv_out_en   : STD_LOGIC;
     SIGNAL conv_out_addr : STD_LOGIC_VECTOR(12 DOWNTO 0);
     SIGNAL conv_out_d    : STD_LOGIC_VECTOR(15 DOWNTO 0);
 
-    TYPE mem_t IS ARRAY (0 TO 9215) OF STD_LOGIC_VECTOR(7 DOWNTO 0);
+
+    TYPE mem_t IS ARRAY (0 TO 8100-1) OF STD_LOGIC_VECTOR(15 DOWNTO 0);
     SIGNAL img_mem : mem_t := (OTHERS => (OTHERS => '0'));
-    SIGNAL mem_read_lat : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL mem_read_lat : STD_LOGIC_VECTOR(15 DOWNTO 0) := (OTHERS => '0');
 
     TYPE omem_t IS ARRAY (0 TO 8100-1) OF STD_LOGIC_VECTOR(15 DOWNTO 0);
     SIGNAL out_mem : omem_t := (OTHERS => (OTHERS => '0'));
@@ -55,14 +57,14 @@ ARCHITECTURE arch OF tb_HESSIAN_conv_r IS
 
 BEGIN
 
-    dut : HESSIAN_conv_r
+    dut : HESSIAN_conv_c
     PORT MAP(
         clk           => clk,
         trg           => trg,
         rdy           => rdy,
-        clahe_in_en   => clahe_in_en,
-        clahe_in_addr => clahe_in_addr,
-        clahe_in_d    => clahe_in_d,
+        conv_in_en    => conv_in_en,
+        conv_in_addr  => conv_in_addr,
+        conv_in_d     => conv_in_d,
         conv_out_en   => conv_out_en,
         conv_out_addr => conv_out_addr,
         conv_out_d    => conv_out_d
@@ -82,13 +84,13 @@ BEGIN
     mem_read_process : PROCESS(clk)
     BEGIN
         IF rising_edge(clk) THEN
-            IF clahe_in_en = '1' THEN
-                mem_read_lat <= img_mem(to_integer(unsigned(clahe_in_addr)));
+            IF conv_in_en = '1' THEN
+                mem_read_lat <= img_mem(to_integer(unsigned(conv_in_addr)));
             END IF;
         END IF;
     END PROCESS;
 
-    clahe_in_d <= mem_read_lat;
+    conv_in_d <= mem_read_lat;
 
     out_write_process : PROCESS(clk)
     BEGIN
@@ -99,7 +101,7 @@ BEGIN
         END IF;
     END PROCESS;
 
-    load_image : PROCESS
+    load_file : PROCESS
         FILE f        : text;
         VARIABLE l    : line;
         VARIABLE val  : INTEGER;
@@ -109,18 +111,18 @@ BEGIN
         file_open(status, f, INPUT_FILE, read_mode);
 
         IF status /= open_ok THEN
-            REPORT "ERROR opening CLAHE input" SEVERITY FAILURE;
+            REPORT "ERROR opening conv_r input file" SEVERITY FAILURE;
         END IF;
 
         WHILE NOT endfile(f) LOOP
             readline(f, l);
             read(l, val);
-            img_mem(idx) <= STD_LOGIC_VECTOR(to_unsigned(val, 8));
+            img_mem(idx) <= STD_LOGIC_VECTOR(to_signed(val, 16));
             idx := idx + 1;
         END LOOP;
 
         file_close(f);
-        REPORT "Loaded " & INTEGER'image(idx) & " pixels";
+        REPORT "Loaded " & INTEGER'image(idx) & " pixels of stage03 conv_r output.";
 
         file_loaded <= true;
         WAIT;
@@ -131,7 +133,7 @@ BEGIN
         WAIT UNTIL file_loaded = true;
         WAIT FOR 100 ns;
 
-        REPORT "Starting HESSIAN convolution...";
+        REPORT "Starting HESSIAN column convolution...";
 
         WAIT UNTIL rising_edge(clk);
         trg <= '1';
@@ -140,7 +142,7 @@ BEGIN
 
         WAIT UNTIL rdy = '1';
 
-        REPORT "Hessian convolution finished.";
+        REPORT "Hessian column convolution finished.";
 
         sim_done <= true;
         WAIT;
@@ -149,6 +151,7 @@ BEGIN
     save_output : PROCESS
         FILE ofile : text;
         VARIABLE l  : line;
+        VARIABLE tmp : INTEGER;
     BEGIN
         WAIT UNTIL rdy = '1';
         WAIT FOR 20 ns;
@@ -156,12 +159,13 @@ BEGIN
         file_open(ofile, OUTPUT_FILE, write_mode);
 
         FOR i IN 0 TO 8099 LOOP
-            write(l, to_integer(unsigned(out_mem(i))));
+            tmp := to_integer(signed(out_mem(i)));
+            write(l, tmp);
             writeline(ofile, l);
         END LOOP;
 
         file_close(ofile);
-        REPORT "Output saved.";
+        REPORT "Column convolution output saved.";
 
         WAIT;
     END PROCESS;
